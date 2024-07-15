c11fcf299ea1a91f7b5a8c919cf0587bc2e684ba
Eileen9876 <104195959+Eileen9876@users.noreply.github.com>
2024-07-15 23:04:00 +0800

    Add files via upload

---
+++ ProgressForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿using System.Windows.Forms;
+
+namespace mylib
+{
+    partial class ProgressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label = new System.Windows.Forms.Label();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 13);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.progressBar.MarqueeAnimationSpeed = 30;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(265, 23);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(11, 40);
+            this.label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(42, 19);
+            this.label.TabIndex = 3;
+            this.label.Text = "label";
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancel_button.Location = new System.Drawing.Point(87, 65);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(114, 26);
+            this.cancel_button.TabIndex = 4;
+            this.cancel_button.Text = "取消";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
+            // 
+            // ProgressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 103);
+            this.ControlBox = false;
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.progressBar);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "ProgressForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "程式執行中";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label;
+        private Button cancel_button;
+    }
+}
---
+++ ProgressForm.cs
@@ -0,0 +1,182 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+
+namespace mylib
+{
+    public partial class ProgressForm : Form
+    {
+        private ProgressTrack Track;
+
+        public ProgressForm(ProgressTrack track)
+        {
+            InitializeComponent();
+
+            this.StartPosition = FormStartPosition.CenterParent;
+
+            label.Text = "";
+
+            Track = track;
+        }
+
+        public void Record_Progress(string msg)
+        {
+            label.Text = msg;
+        }
+
+        public void Set_CancelButtonClick(CancellationTokenSource cts, Task task)
+        {
+            cancel_button.Click += (sender, e) => 
+            {
+                cts.Cancel();
+                task.Wait();
+                this.Close();
+            };
+        }
+
+        private void cancel_button_Click(object sender, EventArgs e)
+        {
+            Track.Cancel();
+        }
+    }
+
+    public class ProgressTrack
+    {
+        private delegate void FormClose();
+
+        private delegate void FormSetMsg(string msg);
+
+        public delegate void Action();
+
+        private Control Control;
+
+        private ProgressForm Form;
+
+        private CancellationTokenSource CTS;
+
+        private Task Task;
+
+        /// <summary>
+        /// 假如發生例外狀況則回傳例外訊息。
+        /// </summary>
+        public Exception GetException { get; private set; }
+
+        public ProgressTrack(Control control)
+        {
+            this.Control = control;
+            this.Form = new ProgressForm(this);
+            this.GetException = null;
+        }
+
+        ~ProgressTrack() 
+        {
+            this.Control = null;
+            this.Form.Dispose();
+            this.Form = null;
+            this.CTS.Dispose(); 
+            this.CTS = null;
+            this.GetException = null;
+        }
+
+        /// <summary>
+        /// 初始並執行。
+        /// </summary>
+        /// <param name="control">父控制項</param>
+        /// <param name="action">任務函式</param>
+        /// <returns></returns>
+        public static ProgressTrack Run(Control control, Action action)
+        {
+            ProgressTrack track = new ProgressTrack(control);
+
+            track.Run(action);
+
+            return track;
+        }
+
+        /// <summary>
+        /// 開始執行。
+        /// </summary>
+        /// <param name="action">任務函式</param>
+        public void Run(Action action)
+        {
+            if (this.CTS != null) this.CTS.Dispose();
+
+            this.CTS = new CancellationTokenSource();
+
+            this.Task = Task.Run(() =>
+            {
+                try
+                {
+                    action();
+                }
+                catch (Exception ex) 
+                {
+                    GetException = ex;
+                }
+                finally
+                {
+                    Close(); //form close
+                }
+            },
+            this.CTS.Token);
+
+            this.Form.Record_Progress("");
+
+            this.Form.ShowDialog(this.Control);
+
+            if (!this.CTS.IsCancellationRequested) this.Task.Wait();
+        }
+
+        /// <summary>
+        /// 發出「取消訊號」。
+        /// </summary>
+        public void Cancel()
+        {
+            if (this.CTS != null && !this.CTS.IsCancellationRequested) this.CTS.Cancel();
+        }
+
+        /// <summary>
+        /// 確認是否有「取消訊號」，假如有則中斷任務並退出，可透過 GetException 接收「取消訊號」產生的例外訊息。
+        /// </summary>
+        public void CancelCheck()
+        {
+            this.CTS.Token.ThrowIfCancellationRequested();
+        }
+
+        /// <summary>
+        /// 關閉視窗
+        /// </summary>
+        public void Close()
+        {
+            if (this.Control.InvokeRequired)
+            {
+                FormClose close = new FormClose(this.Form.Close);
+
+                this.Control.Invoke(close);
+
+                return;
+            }
+
+            this.Form.Close();
+        }
+
+        /// <summary>
+        /// 設置視窗訊息
+        /// </summary>
+        public void SetMsg(string msg)
+        {
+            if (this.Control.InvokeRequired)
+            {
+                FormSetMsg set_msg = new FormSetMsg(this.Form.Record_Progress);
+
+                this.Control.Invoke(set_msg, msg);
+
+                return;
+            }
+
+            this.Form.Record_Progress(msg);
+        }
+    }
+}
+
